29e3cbbb0940eaf99ec02bdb4511d7949bf17928
Zerofran <fran71991@gmail.com>
2023-10-20 14:40:52 -0600

    v4.1

--- HolaAdmin.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Variable de Texto</title>
-</head>
-<body>
-    <h1>Hola Administrador</h1>
-</body>
-</html>
--- HolaCliente.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Variable de Texto</title>
-</head>
-<body>
-    <h1>Hola Cliente</h1>
-</body>
-</html>
