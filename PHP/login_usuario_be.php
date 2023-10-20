34f391e9747f80d98ac3617c82865a0d3563cc54
Zerofran <fran71991@gmail.com>
2023-10-20 15:47:54 -0600

    v1.1.1

--- PHP/login_usuario_be.php
+++ PHP/login_usuario_be.php
@@ -1,95 +1,44 @@
-<?php 
-	session_start();
-	/*---INCLUYENDO LA LLAVE---*/
-	include 'conexion_be.php';
+<?php
+session_start();
 
-	/*---DECLARANDO VARIABLES---*/
-	$correo = $_POST['correo'];
-	$contrasena = $_POST['contrasena'];
+// Incluye la conexión a la base de datos
+include 'conexion_be.php';
 
-	if (empty($correo) or empty($contrasena)) {
-	    $camposFaltantes = array();
+// Obtén los datos del formulario de manera segura
+$correo = isset($_POST['correo']) ? $_POST['correo'] : '';
+$contrasena = isset($_POST['contrasena']) ? $_POST['contrasena'] : '';
 
-	    if (empty($correo)) {
-	        $camposFaltantes[] = "correo";
-	    }
+// Validación de campos obligatorios
+if (empty($correo) || empty($contrasena)) {
+    die("Por favor, complete todos los campos.");
+}
 
-	    if (empty($contrasena)) {
-	        $camposFaltantes[] = "contraseña";
-	    }
+// Aplicar hash y salting a la contraseña
+$contrasena = hash('sha512', $contrasena);
 
-	    $mensaje = "Los siguientes campos no fueron rellenados: " . implode(", ", $camposFaltantes);
+// Consulta preparada para evitar la inyección SQL
+$stmt = mysqli_prepare($conexion, "SELECT * FROM usuarios WHERE correo = ? and contrasena = ?");
+mysqli_stmt_bind_param($stmt, "ss", $correo, $contrasena);
+mysqli_stmt_execute($stmt);
+$resultado = mysqli_stmt_get_result($stmt);
 
-	    echo '
-	        <script>
-	            alert("' . $mensaje . '")
-	            window.location = "../index.php"
-	        </script>
-	    ';
-	    exit();
-	}
+if (mysqli_num_rows($resultado) > 0) {
+    // Inicio de sesión exitoso
+    $_SESSION['usuario'] = $correo;
+    header("location: ../HTML/p_principal.php");
+    exit;
+} else {
+    // Inicio de sesión fallido
+    echo '<script>
+        alert("Inicio de sesión fallido. El usuario no existe o las credenciales son incorrectas.");
+        window.location = "../index.php";
+    </script>';
+    exit;
+}
 
-	//----ENCRIPTADO BASICO DE LA CONTRASEÑA Y CORREO----//
-	$correo = hash('sha512', $correo);
-	$contrasena = hash('sha512', $contrasena);
-	/*-----------------------------------------------------*/
-
-	$validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo' and contrasena = '$contrasena' ");
-
-//esta es la parte que deja entrar a la web a la persona asi que verificar que no este comentada
-// Después de verificar el inicio de sesión exitoso
-	if (mysqli_num_rows($validar_login) > 0) {
-	    $_SESSION['usuario'] = $correo;
-	    header("location: ../HTML/p_principal.php");
-	    exit;
-	} else {
-	    // Si el inicio de sesión falla, muestra un mensaje de error
-	    echo '
-	        <script>
-	            alert("El usuario no existe, por favor cree una cuenta")
-	            window.location = "../index.php"
-	        </script>
-	    '; 
-	    exit;
-	}
-
-//---Comprobando ROl de ususarios---//
-/*
-//verificamos si el usuario a pasado la primera prueba, la de autenticidad de los datos
-	if ($validar_login) {
-	    $usuario = mysqli_fetch_assoc($validar_login); // Obtenemos los datos del usuario en forma de array
-
-	    //ya que la paso verificamos el array anterior y inspeccionamos una parte de la tabla
-	    //para ver si es administrador o cliente
-	    if ($usuario) {
-			if ($usuario['id_cargo'] == 1) { //administrador
-			    header("location: ../HolaAdmin.php");
-			} else if ($usuario['id_cargo'] == 2) { //Cliente
-			   	header("location: ../HolaCliente.php");
-			}else {
-	            echo '
-	                <script>
-	                    alert("El usuario no existe, por favor cree una cuenta")
-	                    window.location = "../index.php"
-	                </script>
-	            ';
-	            exit;
-	        }
-		//si ninguo de los datos coincide entonses el usuario no existe o hay problemas en el array debuelto
-	    } else {
-	        echo '
-	            <script>
-	                alert("El usuario no existe, por favor cree una cuenta")
-	                window.location = "../index.php"
-	            </script>
-	        ';
-	        exit;
-	    }
-	//por si hubo errores en la llave
-	} else {
-	    echo 'Hubo un error en la consulta: ' . mysqli_error($conexion);
-	}
-*/
-//lo anterior fue una prueba nada mas
+// Cerrar la consulta y la conexión
+mysqli_stmt_close($stmt);
+mysqli_close($conexion);
+?>
 
  ?>
