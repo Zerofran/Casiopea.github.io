dd8a5415cb9a079287d6f6d15db4816723993f10
Zerofran <fran71991@gmail.com>
2023-10-20 20:09:04 -0600

    v1.8
    backend de admin correcciones y mas datos

---
+++ Data/negocios.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-10-2023 a las 04:06:51
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `login_registro_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `negocios`
+--
+
+CREATE TABLE `negocios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `ubicacion` varchar(255) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `horario` varchar(100) DEFAULT NULL,
+  `contacto` varchar(100) DEFAULT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `negocios`
+--
+ALTER TABLE `negocios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `negocios`
+--
+ALTER TABLE `negocios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `negocios`
+--
+ALTER TABLE `negocios`
+  ADD CONSTRAINT `negocios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- HTML/admin/subir_informacion.php
+++ HTML/admin/subir_informacion.php
@@ -1,15 +1,10 @@
 <?php
-// Verifica si el formulario se ha enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Conexión a la base de datos (ajusta los valores según tu configuración)
-    $conexion =  mysqli_connect("localhost", "root", "", "login_registro_db");
-
-    // Verifica la conexión a la base de datos
+    $conexion = mysqli_connect("localhost", "root", "", "login_registro_db");
     if ($conexion->connect_error) {
         die("Error en la conexión a la base de datos: " . $conexion->connect_error);
     }
 
-    // Obtiene los datos del formulario
     $nombre = $_POST["nombre"];
     $descripcion = $_POST["descripcion"];
     $ubicacion = $_POST["ubicacion"];
@@ -21,9 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Ruta donde se almacenarán las imágenes (ajusta según tu configuración)
     $ruta_destino = '../../contenedor_img/' . $imagen;
 
-    // Mueve la imagen al directorio de destino
     if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $ruta_destino)) {
-        // Inserta los datos en la base de datos
         $sql = "INSERT INTO negocios (nombre, descripcion, ubicacion, imagen, horario, contacto, usuario_id) VALUES ('$nombre', '$descripcion', '$ubicacion', '$ruta_destino', '$horario', '$contacto', $usuario_id)";
 
         if ($conexion->query($sql) === TRUE) {
@@ -31,11 +24,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             echo "Error al subir la información del negocio: " . $conexion->error;
         }
+
+        // Guarda las imágenes adicionales
+        for ($i = 2; $i <= 4; $i++) {
+            if (!empty($_FILES["imagen$i"]["name"])) {
+                $imagenNombre = $_FILES["imagen$i"]["name"];
+                $rutaDestino = '../../contenedor_img/' . $imagenNombre;
+                if (move_uploaded_file($_FILES["imagen$i"]["tmp_name"], $rutaDestino)) {
+                    $sql = "INSERT INTO imagenes_adicionales (negocio_id, imagen) VALUES (LAST_INSERT_ID(), '$rutaDestino')";
+                    if ($conexion->query($sql) !== TRUE) {
+                        echo "Error al subir la imagen adicional $i: " . $conexion->error;
+                    }
+                } else {
+                    echo "Error al subir la imagen adicional $i.";
+                }
+            }
+        }
     } else {
         echo "Error al subir la imagen.";
     }
 
-    // Cierra la conexión a la base de datos
     $conexion->close();
 }
 ?>
