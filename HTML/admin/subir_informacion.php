3f73ddc8b24c5e079c093189e765bf096c532bb9
Zerofran <fran71991@gmail.com>
2023-10-20 22:33:44 -0600

    v1.3

--- HTML/admin/subir_informacion.php
+++ HTML/admin/subir_informacion.php
@@ -1,4 +1,39 @@
 <?php
+
+session_start();
+
+if (!isset($_SESSION['usuario'])) {
+    echo '
+        <script>
+            alert("Por favor debes iniciar sesión")
+            window.location = "../index.php"
+        </script>
+    ';
+    session_destroy();
+    die();
+}
+
+if (!$conexion) {
+    die('Error en la conexión a la base de datos: ' . mysqli_connect_error());
+}
+
+$correo_encriptado = $_SESSION['usuario'];
+// Realizar la consulta SQL para obtener id_cargo
+$query = "SELECT id_cargo, usuario, id FROM usuarios WHERE correo = '$correo_encriptado'";
+$resultado = mysqli_query($conexion, $query);
+
+if (!$resultado) {
+    echo 'Error en la consulta: ' . mysqli_error($conexion);
+} else {
+    $fila = mysqli_fetch_assoc($resultado);
+    if ($fila) {
+        $id = $fila['id'];
+    } else {
+        echo 'No se encontró un usuario con el correo encriptado.';
+    }
+    mysqli_free_result($resultado);
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conexion = mysqli_connect("localhost", "root", "", "login_registro_db");
     if ($conexion->connect_error) {
@@ -11,14 +46,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $imagen = $_FILES["imagen"]["name"];
     $horario = $_POST["horario"];
     $contacto = $_POST["contacto"];
-    $usuario_id = $_POST["usuario_id"];
+    $usuario_id = $id;
 
     // Ruta donde se almacenarán las imágenes (ajusta según tu configuración)
     $ruta_destino = '../../contenedor_img/';
 
     if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $ruta_destino . $imagen)) {
         // Guarda las imágenes adicionales
-        for ($i = 2; $i <= 4; $i++) {
+        for ($i = 0; $i <= 3; $i++) {
             if (!empty($_FILES["imagen$i"]["name"])) {
                 $imagenNombre = $_FILES["imagen$i"]["name"];
                 if (move_uploaded_file($_FILES["imagen$i"]["tmp_name"], $ruta_destino . $imagenNombre)) {
