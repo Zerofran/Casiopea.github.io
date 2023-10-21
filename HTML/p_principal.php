dd0c288a933bbdaba22480fec43698a66e4c0bab
orlando-mejia <OrlaxGamer@gmail.com>
2023-10-20 18:05:55 -0600

    v1.6
    Correcciones de Header

--- HTML/p_principal.php
+++ HTML/p_principal.php
@@ -7,18 +7,20 @@ include '../PHP/verificarRol_be.php';
 <!DOCTYPE html>
 <html>
 <head>
-
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Pagina principal</title>
 
 	<!-- aqui se linquea hacia la hoja de estilos .css -->
 	<link rel="stylesheet" type="text/css" href="../CSS/style_principal.css">
-
+	<!--Links para las fuentes-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
 </head>
 <body>
-
 	<!--HEADER - MENU-->
+
 	<header>
 		<div class="container__header">
 			<div class="logo">
@@ -38,19 +40,22 @@ include '../PHP/verificarRol_be.php';
 			<div class="menu"> 
 				<nav>
 					<ul>
-						<li><a href="header_botones/hoteles/mostrar_producto.php">Hoteles</a></li>
+						<li><a href="Bontones/chatbot.php">Preguntame!</a></li>
+						<li><a href="Bontones/Hoteles.php">Hoteles</a></li>
 						<li><a href="#">Restaurantes</a></li>
 						<li><a href="#">Gastronomía</a></li>
-						<li><a href="#">rutas</a></li>
+						<li><a href="#">Museos</a></li>
+						<li><a href="#">Rutas</a></li>
 					</ul>
 				</nav>
 				
 				<div class="socialMedia">
-					<a href="admin/ajustes.php">
+					<a href="#">
 						<img src="../IMG/user.png" alt="">
 					</a>
 				</div>
 			</div>
+
 		</div>
 	</header>
 
@@ -68,66 +73,54 @@ include '../PHP/verificarRol_be.php';
 			<!--En esta zona se deben de mostrar los distintos lugares de destino-->
 			<div class="text_head"><h1>Elige tu próximo destino</h1></div>
 			<div class="content_img">
-			    <div class="box_img">
-			        <img src="../IMG/img1.png" alt="Boaco">
-			        <a href="Destinos_grit/Boaco.php" class="btn_zona">Boaco</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img2.png" alt="Carazo">
-			        <a href="Destinos_grit/Carazo.php" class="btn_zona">Carazo</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img3.png" alt="Chinandega">
-			        <a href="Destinos_grit/Chinandega.php" class="btn_zona">Chinandega</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img1.png" alt="Chontales">
-			        <a href="Destinos_grit/Chontales.php" class="btn_zona">Chontales</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img2.png" alt="Estelí">
-			        <a href="Destinos_grit/Estelí.php" class="btn_zona">Estelí</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img3.png" alt="Granada">
-			        <a href="Destinos_grit/Granada.php" class="btn_zona">Granada</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img1.png" alt="Jinotega">
-			        <a href="Destinos_grit/Jinotega.php" class="btn_zona">Jinotega</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img2.png" alt="León">
-			        <a href="Destinos_grit/León.php" class="btn_zona">León</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img3.png" alt="Madriz">
-			        <a href="Destinos_grit/Madriz.php" class="btn_zona">Madriz</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img1.png" alt="Managua">
-			        <a href="Destinos_grit/Managua.php" class="btn_zona">Managua</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img2.png" alt="Masaya">
-			        <a href="Destinos_grit/Masaya.php" class="btn_zona">Masaya</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img3.png" alt="Matagalpa">
-			        <a href="Destinos_grit/Matagalpa.php" class="btn_zona">Matagalpa</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img1.png" alt="Nueva Segovia">
-			        <a href="Destinos_grit/Nueva_Segovia.php" class="btn_zona">Nueva Segovia</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img2.png" alt="Río San Juan">
-			        <a href="Destinos_grit/Río_San_Juan.php" class="btn_zona">Río San Juan</a>
-			    </div>
-			    <div class="box_img">
-			        <img src="../IMG/img3.png" alt="Rivas">
-			        <a href="Destinos_grit/Rivas.php" class="btn_zona">Rivas</a>
-			    </div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="Destinos_grit/Granada.php" class="btn_zona">Granada</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">León</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">Masaya</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">Rivas</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">Matagalpa</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">Managua</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">jinotega</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">Boaco</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">Estelí</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">Río San Juan</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">Carazo</a>
+				</div>
+				<div class="box_img">
+					<img src="../IMG/img2.png" alt="">
+					<a href="#" class="btn_zona">Chinandega</a>
+				</div>
 			</div>
 
 			<!--carrucel de destinos recomendados segun el viaje-->
@@ -177,34 +170,32 @@ include '../PHP/verificarRol_be.php';
 			    <button class="btn_carrucel"id="nextBtn">Siguiente</button>
 			</div>
 		</div>
-		<!--Pie de paguina-->
-		<footer class="piePagina">
-			<h1>Este es el pie de paguina</h1>
-			<h2>Lorem Impush</h2>
+		<!--Inicio Pie de pagina-->
+		<footer class="footer">
+  			<div class="imagotipo"><img src="../IMG/imagotipo_negativo.png"/></div>
+  			<div class="social-icons">
+    			<img src="../IMG/Icons/Facebook.svg" alt="Facebook Icon"/>
+				<img src="../IMG/Icons/Instagram.svg" alt="Instagram Icon"/>
+				<img src="../IMG/Icons/Twitter.svg" alt="Twitter Icon"/>
+  			</div>
+  			<div class="location-text">
+    			Ubicacion <br>
+    			App Nica Destiny
+  			</div>
+  			<div class="copyright">
+   				&copy; 2023 Tu Empresa
+  			</div>
+  			<div class="menu">
+   				<a href="#">Políticas</a> |
+    			<a href="#">Privacidad</a> |
+    			<a href="#">Cookies</a>
+  			</div>
 		</footer>
 
+		<!--Final Pie de pagina-->
 	</main>
 	<a href="../PHP/cerrar_sesion.php" class="cerrar-sesion">Cerrar Sesión</a>
 	<script type="text/javascript" src="../JS/carrusel.js"></script>
 	<script type="text/javascript" src="../JS/menuAmburgesa.js"></script>
-
-	<script type="text/javascript">
-	  // En cualquier otro script en la misma página
-	//se puede llamar de manera normal pero estoe es por si el php se ejecuta muy tarde
-	  function waitForCargo() {
-	    return new Promise((resolve) => {
-	      if (window.id_cargo !== undefined) {
-	        resolve(window.id_cargo);
-	      } else {
-	        setTimeout(() => waitForCargo().then(resolve), 100);
-	      }
-	    });
-	  }
-
-	  waitForCargo().then((cargo) => {
-	    console.log("El valor de id_cargo es: " + cargo);
-	    // Puedes realizar acciones que dependan de cargo aquí
-	  });
-	</script>
 </body>
 </html>
