860cc5906eb1578990ce794283e51f9b03c160b9
Zerofran <fran71991@gmail.com>
2023-10-20 21:00:03 -0600

    v1.2
    backend

--- HTML/admin/subir_informacion.php
+++ HTML/admin/subir_informacion.php
@@ -14,25 +14,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario_id = $_POST["usuario_id"];
 
     // Ruta donde se almacenarán las imágenes (ajusta según tu configuración)
-    $ruta_destino = '../../contenedor_img/' . $imagen;
+    $ruta_destino = '../../contenedor_img/';
 
-    if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $ruta_destino)) {
-        $sql = "INSERT INTO negocios (nombre, descripcion, ubicacion, imagen, horario, contacto, usuario_id) VALUES ('$nombre', '$descripcion', '$ubicacion', '$ruta_destino', '$horario', '$contacto', $usuario_id)";
-
-        if ($conexion->query($sql) === TRUE) {
-            echo "Información del negocio subida con éxito.";
-        } else {
-            echo "Error al subir la información del negocio: " . $conexion->error;
-        }
-
+    if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $ruta_destino . $imagen)) {
         // Guarda las imágenes adicionales
         for ($i = 2; $i <= 4; $i++) {
             if (!empty($_FILES["imagen$i"]["name"])) {
                 $imagenNombre = $_FILES["imagen$i"]["name"];
-                $rutaDestino = '../../contenedor_img/' . $imagenNombre;
-                if (move_uploaded_file($_FILES["imagen$i"]["tmp_name"], $rutaDestino)) {
-                    $sql = "INSERT INTO imagenes_adicionales (negocio_id, imagen) VALUES (LAST_INSERT_ID(), '$rutaDestino')";
-                    if ($conexion->query($sql) !== TRUE) {
+                if (move_uploaded_file($_FILES["imagen$i"]["tmp_name"], $ruta_destino . $imagenNombre)) {
+                    // Inserta los datos en la base de datos
+                    $sql = "INSERT INTO negocios (nombre, descripcion, ubicacion, imagen, horario, contacto, usuario_id) VALUES ('$nombre', '$descripcion', '$ubicacion', '$imagen', '$horario', '$contacto', $usuario_id)";
+
+                    if ($conexion->query($sql) === TRUE) {
+                        echo "Información del negocio subida con éxito.";
+                    } else {
+                        echo "Error al subir la información del negocio: " . $conexion->error;
+                    }
+
+                    // Ahora, inserta las imágenes adicionales en la misma fila del negocio
+                    $negocio_id = mysqli_insert_id($conexion);
+                    $sqlImagenes = "UPDATE negocios SET imagen$i = '$imagenNombre' WHERE id = $negocio_id";
+                    if ($conexion->query($sqlImagenes) !== TRUE) {
                         echo "Error al subir la imagen adicional $i: " . $conexion->error;
                     }
                 } else {
@@ -41,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
     } else {
-        echo "Error al subir la imagen.";
+        echo "Error al subir la imagen principal.";
     }
 
     $conexion->close();
